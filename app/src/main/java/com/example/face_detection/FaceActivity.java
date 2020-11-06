fd1db9ed9769ed4733db30dc189bb2b4f205006b
Leedw <idowon@idowon-ui-iMac.local>
2020-11-06 19:24:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/face_detection/FaceActivity.java
@@ -0,0 +1,104 @@
+package com.example.face_detection;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionPoint;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceContour;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceLandmark;
+
+import java.util.List;
+
+public class FaceActivity extends Activity {
+
+    Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_face);
+        mContext = this;
+
+        final RelativeLayout RelativeLayout_main = findViewById(R.id.RelativeLayout_main);
+
+        // High-accuracy landmark detection and face classification
+        FirebaseVisionFaceDetectorOptions options =
+                new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                        .build();
+
+        final Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.aa);
+
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+
+        FirebaseVisionFaceDetector detector = FirebaseVision.getInstance()
+                .getVisionFaceDetector(options);
+
+        Task<List<FirebaseVisionFace>> result =
+                detector.detectInImage(image)
+                        .addOnSuccessListener(
+                                new OnSuccessListener<List<FirebaseVisionFace>>() {
+                                    @Override
+                                    public void onSuccess(List<FirebaseVisionFace> faces) {
+                                        // Task completed successfully
+
+                                        Log.d("FACES", faces.toString());
+
+                                        Point p = new Point();
+                                        Display display = getWindowManager().getDefaultDisplay();
+                                        display.getSize(p);
+
+                                        for (FirebaseVisionFace face : faces) {
+
+                                            // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
+                                            // nose available):
+                                            FirebaseVisionFaceLandmark leftEye = face.getLandmark(FirebaseVisionFaceLandmark.NOSE_BASE);
+                                            float lex = leftEye.getPosition().getX();
+                                            float ley = leftEye.getPosition().getY();
+
+                                            ImageView imageLE = new ImageView(mContext);
+                                            imageLE.setImageResource(R.drawable.mung);
+                                            imageLE.setX(p.x * lex / bitmap.getWidth()-50);
+                                            imageLE.setY(p.y * ley / bitmap.getHeight()-380);
+
+                                            imageLE.setLayoutParams(new RelativeLayout.LayoutParams(100,100));
+
+                                            RelativeLayout_main.addView(imageLE);
+
+                                        }
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        // ...
+                                    }
+                                });
+
+    }
+}
